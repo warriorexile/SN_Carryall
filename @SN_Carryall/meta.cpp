329d223c92e6b575513ad20acda6e1fbd91318c1
warriorexile <chrisbryan81@gmail.com>
2018-12-06 23:23:45 +0000

    Add files via upload

---
+++ @SN_Carryall/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1201267284;
+name = "SN Carryall Backpack Plus";
+timestamp = 5248372828041838051;
---
+++ @SN_Carryall/mod.cpp
@@ -0,0 +1,6 @@
+name = "SN Carryall Backpack Plus";
+picture = "mod.paa";
+author = "+S+";
+actionName = "Website";
+action = "http://steamcommunity.com/sharedfiles/filedetails/?id=1201267284";
+overview = "Carry more with the improved Carryall Backpacks!";
